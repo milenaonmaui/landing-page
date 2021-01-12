a345d0606d87614c56b224bd549cc7f3fe08dab5
Milena Kari <milenakari@yahoo.com>
2021-01-12 11:32:30 -1000

    Convert functions to arrow functions. clear debug comments.

--- js/app.js
+++ js/app.js
@@ -17,30 +17,27 @@
  * Define Global Variables
  * 
 */
-//const fragment = document.createDocumentFragment();
 
 const navList = document.querySelector('#navbar__list');
 const sectionsList = document.querySelectorAll('section');
 let currActiveSection = document.getElementsByClassName('section__active')[0];
-console.log(sectionsList)
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-function deactivateSection(section){  
-    section.classList.remove('section__active')
-    const navLink = document.getElementById("nav-" + section.id)
-    console.log("Nav Link ", navLink)
+const deactivateSection = (section) => {  
+    section.classList.remove('section__active');
+    const navLink = document.getElementById("nav-" + section.id);
     navLink.classList.remove('nav__active');
-}
+};
 
-function activateSection(section) {
-    section.classList.add('section__active')
-    const navLink = document.getElementById("nav-" + section.id)
+const activateSection = (section) => {
+    section.classList.add('section__active');
+    const navLink = document.getElementById("nav-" + section.id);
     navLink.classList.add('nav__active');
-}
+};
 
 
 /**
@@ -49,51 +46,45 @@ function activateSection(section) {
  * 
 */
 
-// build the nav removing link to active section
+// build the navigation bar
 
-function buildNav() {
+const buildNav = () => {
     for (const section of sectionsList){       
         const newListItem = document.createElement('li');
         const navLink = document.createElement('a');
         const navLinkText = document.createTextNode(section.dataset.nav);
         navLink.appendChild(navLinkText);
         navLink.href = '#' + section.id;
-        navLink.addEventListener('click', scrollToAnchorId)        
-        newListItem.appendChild(navLink)
-        newListItem.className = "menu__link"
+        navLink.addEventListener('click', scrollToAnchorId);      
+        newListItem.appendChild(navLink);
+        newListItem.className = "menu__link";
         newListItem.id = "nav-" + section.id;
-        navList.appendChild(newListItem)
+        navList.appendChild(newListItem);
     }
-}
+};
 
 
 // Add class 'active' to section when near top of viewport
 
-function handleActiveContent(event) {
-    console.log("Inside active handler")
+const handleActiveContent = (event) => {
     for (const section of sectionsList) {
-        const sectionTop = section.getBoundingClientRect().top
-        console.log("Section ", section.id, "offset ", sectionTop)
-        if (sectionTop > -1 && sectionTop < window.visualViewport.height/2){
-            deactivateSection(currActiveSection)
-            activateSection(section)
+        const sectionTop = section.getBoundingClientRect().top;
+        if (sectionTop > -1 && sectionTop < window.visualViewport.height/2) {
+            deactivateSection(currActiveSection);
+            activateSection(section);
             currActiveSection = section;
         }
     }
- }
-
-
+ };
 
 // Scroll to anchor ID using scrollTO event
 
-function scrollToAnchorId(event){
+const scrollToAnchorId = (event) => {
     event.preventDefault();
     const sectionId = event.target.href.split("#")[1];
     const section = document.getElementById(sectionId);
-    //window.scrollTo(0,section.getBoundingClientRect().top)
-    window.scrollTo(0,section.offsetTop)
-    //console.log(sectionId, section.getBoundingClientRect().top)
-}
+    window.scrollTo(0,section.offsetTop);
+};
 /**
  * End Main Functions
  * Begin Events
@@ -104,7 +95,3 @@ function scrollToAnchorId(event){
 
 buildNav()
 window.addEventListener('scroll', handleActiveContent)
-
-// Scroll to section on link click
-
-// Set sections as active
